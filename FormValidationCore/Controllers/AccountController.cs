4587fe3329fe8783852dc39fcc6492bf5ef1aad3
athifhere <athifhere@gmail.com>
2022-01-19 14:29:12 +0530

    Add project files.

---
+++ FormValidationCore/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FormValidationCore.Models;
+
+namespace FormValidationCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            UserModel model = new UserModel();
+            model.CountriesList = new List<string>() { "India", "USA", "UK" };
+            TempData["CountriesList"] = model.CountriesList;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserModel model)
+        {
+            model.CountriesList = new List<string>();
+            foreach (var item in (IEnumerable<string>)TempData["CountriesList"])
+            {
+                model.CountriesList.Add(item);
+            }
+            TempData["Username"] = model.UserName;
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Message");
+            }
+            return View(model);
+        }
+
+        public IActionResult Message()
+        {
+            ViewBag.Username = (string)TempData["Username"];
+            return View();
+        }
+
+        public List<string> GetCityListByCountry(string countryName)
+        {
+            List<string> CityList = new List<string>() { "New Delhi", "Chennai", "Hyderabad", "Mumbai", "Kolkata",
+            "New York", "Los Angeles", "Boston", "Houston", "Washington",
+            "London", "Manchester", "Liverpool", "Birmingham", "Edinburgh"};
+            List<string> data = new List<string>();
+
+            if (countryName == "India")
+            {
+                data = CityList.GetRange(0, 5);
+                data.Sort();
+            }
+            else if (countryName == "USA")
+            {
+                data = CityList.GetRange(5, 5);
+                data.Sort();
+            }
+            else if (countryName == "UK")
+            {
+                data = CityList.GetRange(10, 5);
+                data.Sort();
+            }
+            return data;
+        }
+    }
+}
---
+++ FormValidationCore/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FormValidationCore.Attributes;
+
+namespace FormValidationCore.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your username")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm the password")]
+        [Compare("Password", ErrorMessage = "The passwords dont match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter contact details")]
+        [RegularExpression("^[789]\\d{9}$", ErrorMessage = "Enter the contact details in correct format")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Please select your country")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Please select your city")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please select your gender")]
+        public string Gender { get; set; }
+
+        [ValidateCheckBox(ErrorMessage = "Please accept terms and conditions")]
+        public bool AcceptTerms { get; set; }
+
+        public List<string>? CountriesList { get; set; }
+    }
+}
